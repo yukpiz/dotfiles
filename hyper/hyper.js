313904fbc2e9bd4ab87b5c2488eb014f427d1f73
yukpiz <yukpiz@gmail.com>
2018-08-01 18:13:01 +0900

    Change padding.

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -61,7 +61,7 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: '7px 3px 3px 3px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
@@ -131,12 +131,14 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hyperemoji"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: [],
+  localPlugins: [
+    'hyperemoji',
+  ],
 
   keymaps: {
     // Example
